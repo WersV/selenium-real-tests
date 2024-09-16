07c504645578de6843dd4816ab2fd4b6ef0d7cd1
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-09-16 22:33:21 +0200

    fix bug with click on register btn

--- src/test/java/com/seleniumdemo/pages/MyAccPage.java
+++ src/test/java/com/seleniumdemo/pages/MyAccPage.java
@@ -1,10 +1,14 @@
 package com.seleniumdemo.pages;
 
 import com.seleniumdemo.utils.SeleniumHelper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MyAccPage {
@@ -36,7 +40,9 @@ public class MyAccPage {
     }
 
     public void clickRegisterBtn() {
-        registerBtn.click();
+//        registerBtn.click();
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("arguments[0].click()", registerBtn);
     }
 
 }
--- src/test/java/com/seleniumdemo/tests/RegisterTest.java
+++ src/test/java/com/seleniumdemo/tests/RegisterTest.java
@@ -2,6 +2,12 @@ package com.seleniumdemo.tests;
 
 import com.seleniumdemo.pages.HomePage;
 import com.seleniumdemo.pages.MyAccPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RegisterTest extends BaseTest {
@@ -15,6 +21,10 @@ public class RegisterTest extends BaseTest {
         MyAccPage myAccPage = new MyAccPage(driver);
         myAccPage.enterEmail();
         myAccPage.enterPassword();
+
         myAccPage.clickRegisterBtn();
+
+        WebElement myAccH1 = driver.findElement(By.xpath("//h1[@class='entry-title' and text()='My account']"));
+        Assert.assertEquals(myAccH1.getText(), "My account");
     }
 }
