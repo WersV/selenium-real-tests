c4d008756114085eed8e512d9ff974db01692542
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-10-14 22:13:27 +0200

    add dataprovider to PurchaseTest

--- src/test/java/com/seleniumdemo/tests/PurchaseTest.java
+++ src/test/java/com/seleniumdemo/tests/PurchaseTest.java
@@ -2,9 +2,13 @@ package com.seleniumdemo.tests;
 
 import com.seleniumdemo.models.Customer;
 import com.seleniumdemo.pages.*;
+import com.seleniumdemo.utils.ExcelReader;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class PurchaseTest extends BaseTest {
 
@@ -26,6 +30,46 @@ public class PurchaseTest extends BaseTest {
         .viewCartBtnClick()
         .proceedToCheckoutBtnClick()
         .fillAddressDetails(customer);
+
+        OrderPlacedPage orderPlaced = new OrderPlacedPage(driver);
+        WebElement orderReceivedMsg = orderPlaced.getOrderReceivedMsg();
+        Assert.assertEquals(orderReceivedMsg.getText(), "Thank you. Your order has been received.");
+
+        WebElement orderProductName = orderPlaced.getProductName();
+        Assert.assertEquals(orderProductName.getText(), "Java Selenium WebDriver");
+    }
+
+    @DataProvider
+    public Object[][] testData() throws IOException {
+        return ExcelReader.readExcel("testData.xlsx");
+    }
+
+    @Test(dataProvider = "testData")
+    public void purchaseProductTestWithDataProvider(String email, String passw, String firstName, String lastName, String companyName, String country, String address, int zipCode, String city, int phone, String orderNotes) throws InterruptedException {
+        driver.get("http://seleniumdemo.com/");
+
+        String productAddSuccessText = new HomePage(driver)
+                .clickShopBtn()
+                .openProduct("Java Selenium WebDriver")
+                .addToCartBtnClick()
+                .divEl().getText();
+
+        Assert.assertTrue(productAddSuccessText.contains("“Java Selenium WebDriver” has been added to your cart."));
+
+        Customer customer = new Customer();
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customer.setCountry(country);
+        customer.setAddress(address);
+        customer.setZipCode(String.valueOf(zipCode));
+        customer.setCity(city);
+        customer.setPhoneNum(String.valueOf(phone));
+        customer.setEmail(email);
+
+        new ProductDetailsPage(driver)
+                .viewCartBtnClick()
+                .proceedToCheckoutBtnClick()
+                .fillAddressDetails(customer);
 
         OrderPlacedPage orderPlaced = new OrderPlacedPage(driver);
         WebElement orderReceivedMsg = orderPlaced.getOrderReceivedMsg();
--- src/test/java/com/seleniumdemo/utils/ExcelReader.java
+++ src/test/java/com/seleniumdemo/utils/ExcelReader.java
@@ -39,6 +39,7 @@ public class ExcelReader {
                     } else if (cell.getCellType() == CellType.NUMERIC) {
                         // If it's numeric, we convert it to String for consistency
                         data[i - 1][j] = (int) cell.getNumericCellValue();
+                        System.out.println((int) cell.getNumericCellValue());
                     } else {
                         data[i - 1][j] = cell.toString();  // Default case, just in case
                     }
@@ -54,6 +55,6 @@ public class ExcelReader {
     }
 
     public static void main(String[] args) throws IOException {
-        readExcel("logInData.xlsx");
+        readExcel("testData.xlsx");
     }
 }
